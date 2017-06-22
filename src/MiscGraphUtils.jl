716f3edf4f3172075d3c6aaf514841e48540e3f6
Seth Bromberger <github@bromberger.com>
2017-06-22 11:45:59 -0700

    initial commit

--- src/MiscGraphUtils.jl
+++ src/MiscGraphUtils.jl
@@ -1,5 +1,15 @@
 module MiscGraphUtils
 
-# package code goes here
+using GraphIO
+using LightGraphs
 
+export graphcat
+
+function graphcat(g)
+           io = IOBuffer()
+           GraphIO.savegml(io, g)
+           graphstr = String(io)
+           p = pipeline(`echo $graphstr`, `gml2gv`, `dot -Tpng`, `imgcat`)
+           run(p)
+end
 end # module
